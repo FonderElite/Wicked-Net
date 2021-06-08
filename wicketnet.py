04327ffc3cf4278b8f115635647cfbf6acc6b20b
FonderElite <s1170044@usls.edu.ph>
2021-06-08 05:26:44 +0000

    Update wicketnet.py

--- wicketnet.py
+++ wicketnet.py
@@ -1,4 +1,4 @@
-import os,argparse,logging,time,sys,re,subprocess
+import os,argparse,logging,time,sys,re,subprocess,queue
 from threading import Event
 from multiprocessing import Process
 import scapy.all as scapy
@@ -20,7 +20,7 @@ class Extract(object):
     @staticmethod 
     def show_banner(s):
         for c in s + '\n':
-            sys.stdout.write(Fore.WHITE + c)
+            sys.stdout.write(bg.BLACK + Fore.WHITE + c)
             sys.stdout.flush()
             time.sleep(2. / 100)
         print('Made By FonderElite')
@@ -32,13 +32,15 @@ class Extract(object):
             m, s = divmod(stop, 60)
             h, m = divmod(m, 60)
             timeleft = Fore.RED + "Time-out: " + Fore.WHITE + str(h).zfill(2) + ":" + str(m).zfill(2) + ":" + str(s).zfill(2)
-            print(f"\r", end=timeleft)
-            print("\n")
+            print("\r", end=timeleft)
             time.sleep(1)
             stop -= 1
     def main(self):
         try:     
             ip_add_range_pattern = re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+            logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+            logging.info('Wicked-Net is running.')
+            time.sleep(2)
             while True:
                 ip_add_range_entered = self.ip + '/' + self.drange
                 if ip_add_range_pattern.search(ip_add_range_entered):
@@ -53,7 +55,7 @@ class Extract(object):
            print('Use -h for help')
     @staticmethod
     def scan_devices():
-        print(f"{Fore.WHITE}[{Fore.GREEN}+{Fore.WHITE}]Devices' IP in your Local Area Network(LAN): ")
+        print(f"\n{Fore.WHITE}[{Fore.GREEN}+{Fore.WHITE}]Devices' IP in your Local Area Network(LAN): ")
         cmd = '''
 #!/bin/sh
 for i in {1..255}
@@ -70,23 +72,30 @@ done
         else:
             pass
 if __name__ == '__main__':
-    obj_class = Extract(args.ipaddress,args.devicerange,args.timeout)
-    banner = Process(target=obj_class.show_banner, args=('''
+    try:
+        q = queue.Queue()
+        t = time.time()
+        obj_class = Extract(args.ipaddress,args.devicerange,args.timeout)
+        banner = Process(target=obj_class.show_banner, args=('''
  _         ___      _            _       _   _      _       *  .  . *       *    .        .        .   *    ..
  \ \      / (_) ___| | _____  __| |     | \ | | ___| |_      .   __      *        .        .      .        .            *
   \ \ /\ / /| |/ __| |/ / _ \/ _` |_____|  \| |/ _ \ __| *   ___( o)> '   *     *.   *       .     *      *        *    .
    \ V  V / | | (__|   <  __/ (_| |_____| |\  |  __/ |_      \ <_. )     *     *      ' *       * '     *  ' '      * '
     \_/\_/  |_|\___|_|\_\___|\__,_|     |_| \_|\___|\__|       ---'  
 ''',))
-    countdown_obj = Process(target=obj_class.count_down)
-    scan_obj = Process(target=obj_class.scan_devices)
-    process = Process(target=obj_class.main)
-    banner.start()
-    banner.join()
-    process.start()
-    process.join(timeout=int(args.timeout))
-    countdown_obj.start()
-    countdown_obj.join()
-    # We send a signal that the other thread should stop.
-    process.terminate()
-    scan_obj.start()
+        countdown_obj = Process(target=obj_class.count_down)
+        scan_obj = Process(target=obj_class.scan_devices)
+        process = Process(target=obj_class.main)
+        banner.start()
+        banner.join()
+        process.start()
+        process.join(timeout=int(args.timeout))
+        countdown_obj.start()
+        process.terminate()
+        countdown_obj.join()
+        # We send a signal that the other thread should stop.
+        scan_obj.start()
+        scan_obj.join()
+        print("Done in: {}".format(time.time()-t) + 's')
+    except Exception as Err:
+        print(Err)
